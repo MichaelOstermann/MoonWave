c01fe08b8cb407bea51fcab80bda0c738f7048ae
Michael Ostermann <michaelostermann@me.com>
2025-02-11 13:45:52 +0100

    feat: improve tracklist refocus behaviour

--- src/actions/onClickTrack.ts
+++ src/actions/onClickTrack.ts
@@ -1,12 +1,16 @@
 import type { MouseEvent } from 'react'
 import { $focusedView, $tracksLSM } from '@app/state/state'
 import { handleMouseEvent } from '@app/utils/lsm/utils/handleMouseEvent'
+import { isSelected } from '@app/utils/lsm/utils/isSelected'
 import { action } from '@app/utils/signals/action'
 
 export const onClickTrack = action(({ evt, trackId }: {
     evt: MouseEvent
     trackId: string
 }) => {
+    const wasFocused = $focusedView() === 'MAIN'
     $focusedView.set('MAIN')
+
+    if (!wasFocused && isSelected($tracksLSM(), trackId)) return
     $tracksLSM.map(lsm => handleMouseEvent(lsm, trackId, evt.nativeEvent))
 })
