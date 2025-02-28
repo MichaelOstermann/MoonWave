51b4c8277db8bae315f87b2b3759c4a9ebc32594
Michael Ostermann <michaelostermann@me.com>
2025-02-28 23:02:05 +0100

    feat: tweak tooltip behavior

--- src/utils/modals/components/TooltipTarget.tsx
+++ src/utils/modals/components/TooltipTarget.tsx
@@ -1,7 +1,8 @@
 import type { Tooltip } from '../types'
 import { useSignal } from '@app/utils/signals/useSignal'
 import { Slot } from '@radix-ui/react-slot'
-import { type ComponentProps, type ReactNode, useEffect, useState } from 'react'
+import { useUnmountEffect } from '@react-hookz/web'
+import { type ComponentProps, type ReactNode, useRef } from 'react'
 
 interface TooltipTargetProps extends Omit<ComponentProps<'div'>, 'popover'> {
     tooltip: Tooltip
@@ -11,19 +12,15 @@ interface TooltipTargetProps extends Omit<ComponentProps<'div'>, 'popover'> {
 
 export function TooltipTarget({
     tooltip,
-    delay = 800,
+    delay = 600,
     asChild,
     ...rest
 }: TooltipTargetProps): ReactNode {
     const Comp = asChild ? Slot : 'div'
     const status = useSignal(tooltip.status)
-    const [isHovered, setIsHovered] = useState(false)
+    const timerRef = useRef<Timer>(undefined)
 
-    useEffect(() => {
-        if (!isHovered) return tooltip.close()
-        const tid = setTimeout(tooltip.open, delay)
-        return () => clearTimeout(tid)
-    }, [tooltip, isHovered, delay])
+    useUnmountEffect(() => clearTimeout(timerRef.current))
 
     return (
         <Comp
@@ -31,8 +28,15 @@ export function TooltipTarget({
             ref={el => void tooltip.anchorElement.set(el)}
             data-modal-target="tooltip"
             data-modal-status={status}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
+            onMouseDown={() => clearTimeout(timerRef.current)}
+            onMouseMove={() => {
+                clearTimeout(timerRef.current)
+                timerRef.current = setTimeout(tooltip.open, delay)
+            }}
+            onMouseLeave={() => {
+                clearTimeout(timerRef.current)
+                tooltip.close()
+            }}
         />
     )
 }
