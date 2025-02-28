97bd893734d9a953c7d0229d96d7e301c6673254
Michael Ostermann <michaelostermann@me.com>
2025-02-28 22:50:51 +0100

    fix: prevent flickering when track finishes

--- src/actions/onAudioPause.ts
+++ src/actions/onAudioPause.ts
@@ -1,6 +1,14 @@
-import { $playing } from '@app/state/state'
+import { $loadedAudioMetadata, $playing, audio } from '@app/state/state'
 import { action } from '@app/utils/signals/action'
 
 export const onAudioPause = action(() => {
-    $playing.set(false)
+    setTimeout(() => {
+        // We are loading or playing the next track, skip.
+        // This happens when reaching the end of a track - first the `pause` event fires,
+        // immediately followed by `ended` which loads the next track asynchronously.
+        if (!$loadedAudioMetadata()) return
+        if (!audio.paused) return
+
+        $playing.set(false)
+    }, 0)
 })
